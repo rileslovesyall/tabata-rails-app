b142c0fbf443f6e303687937a0e2bd8e113fcfc1
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-24 14:56:28 -0800

    changed loging logout routes to have no dash

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   	get '/' => 'home#index'
 
-  	get "/log-in" => "sessions#new"
-  	post "/log-in" => "sessions#create"
-  	get "/log-out" => "sessions#destroy", as: :log_out
+  	get "/login" => "sessions#new"
+  	post "/login" => "sessions#create"
+  	get "/logout" => "sessions#destroy", as: :log_out
 
   	resources :users
   	resources :workouts
