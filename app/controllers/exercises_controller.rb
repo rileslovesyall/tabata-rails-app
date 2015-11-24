fcdf9101334bcedab56316d7a889dc7d24e1a057
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-24 12:24:10 -0800

    updated controllers for CRUD actions, created migration for exercisesworkuts join table"

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,34 @@
 class ExercisesController < ApplicationController
+
+
+	def index
+		@exercises = Exercise.all
+	end
+
+	def show
+		@exercise = Exercise.find(params[:id])
+	end
+
+	def new
+		@exercise = Exercise.new
+	end
+
+	def create
+		@exercise = Exercise.new(exercise_params)
+		if @exercise.save
+			flash[:notice] = "You made a new exercise!"
+			redirect_to exercises_path
+		else
+			flash[:alert] = "There was a problem creating this exercise. Please try again."
+			redirect_to :back
+		end
+	end
+
+	private
+
+	def exercise_params
+		params.require(:exercise).permit(:name, :body_focus)
+	end
+
+
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,33 @@
 class WorkoutsController < ApplicationController
+
+	def index
+		@workouts = Workout.all
+	end
+
+	def show
+		@workout = Workout.find(params[:id])
+	end
+
+	def new
+		@workout = Workout.new
+	end
+
+	def create
+		@workout = Workout.new(workout_params)
+		if @workout.save
+			flash[:notice] = "Your workout has been saved!"
+			redirect_to user_path
+		else
+			flash[:alert] = "There was a problem creating your account. Please try again."
+			redirect_to :back
+		end
+	end
+
+	private
+
+	def workout_params
+		params.require(:workout).permit(:name)
+	end
+
+
 end
