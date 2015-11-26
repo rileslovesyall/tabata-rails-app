9c533ca1473ec6da6143a00a6ebb236e180c0ca3
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-26 15:52:11 -0800

    workout generator method working properly

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,8 +4,11 @@ class Workout < ActiveRecord::Base
 	has_many :exercises_workouts
 
 	def self.generate 
-		exercise1 = Exercise.find(rand(0..(Exercise.length-1)))
-		exercise2 = Exercise.find(rand(0..(Exercise.length-1))) until exercise2 != exercise1
+		exercise1 = Exercise.find(rand(1..(Exercise.all.length)))
+		exercise2 = Exercise.find(rand(1..(Exercise.all.length)))
+		until exercise1 != exercise2
+			exercise2 = Exercise.find(rand(1..(Exercise.all.length)))
+		end
 		generated_exs = [exercise1, exercise2]
 		return generated_exs
 	end
