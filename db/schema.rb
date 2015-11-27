d6035f502cdd49abf04c43a6d59cae90f7acb94e
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-27 09:36:59 -0800

    Added description to workouts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127003317) do
+ActiveRecord::Schema.define(version: 20151127173618) do
 
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
@@ -39,9 +39,10 @@ ActiveRecord::Schema.define(version: 20151127003317) do
 
   create_table "workouts", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "description"
   end
 
 end
