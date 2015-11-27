6fb6895d783b8a1cf3d8a274fd9bc37f35a030b7
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-26 17:07:01 -0800

    maybe workout#create is working?

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,8 +3,12 @@ class WorkoutsController < ApplicationController
 	def index
 		@workouts = Workout.all
 		x = params[:num_ex].to_i if !params[:num_ex].nil?
-		x = 3 if params[:num_ex].nil?
+		x = 0 if params[:num_ex].nil?
 		@generator = Workout.generate(x)
+		@ids = []
+		@generator.each do |ex|
+			@ids.push(ex.id)
+		end
 	end
 
 	def show
@@ -17,12 +21,11 @@ class WorkoutsController < ApplicationController
 
 	def create
 		@workout = Workout.new(workout_params)
-		if @workout.save
-			flash[:notice] = "Your workout has been saved!"
-			redirect_to user_path
-		else
-			flash[:alert] = "There was a problem creating your account. Please try again."
-			redirect_to :back
+		exs_string = params[:exs]
+		ex_id_arr = exs_string.split(' ')
+		ex_id_arr.each do |ex|
+			x = Exercise.find(ex)
+			@workout.exercises <<  x
 		end
 	end
 
