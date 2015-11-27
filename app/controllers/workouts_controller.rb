e603ceb73aa6ac725f0414b83b23ca797735b74a
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-26 21:11:01 -0800

    added delete link on user#show and delete is functional

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -33,6 +33,11 @@ class WorkoutsController < ApplicationController
 		redirect_to workouts_path
 	end
 
+	def destroy
+		Workout.delete(params[:id])
+		redirect_to user_path(session[:user_id])
+	end
+
 	private
 
 	def workout_params
